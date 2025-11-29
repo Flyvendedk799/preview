ee8f5160a91bf3234fc0baea0469b33c03c5a0f8
Tobias Preisler Mastek <tobiasflyvende@gmail.com>
2025-11-29 21:55:42 +0100

    Fix migration: Shorten revision ID to fit VARCHAR(32) constraint

--- backend/migrations/versions/20250201_add_description_to_preview.py
+++ backend/migrations/versions/20250201_add_description_to_preview.py
@@ -1,6 +1,6 @@
 """add description to preview
 
-Revision ID: 20250201_add_description_to_preview
+Revision ID: 20250201_add_desc
 Revises: 20241123_01_add_user_id_columns
 Create Date: 2025-02-01 00:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '20250201_add_description_to_preview'
+revision: str = '20250201_add_desc'
 down_revision: Union[str, None] = '20241123_01_add_user_id_columns'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- backend/migrations/versions/20250213_add_metadata_fields_to_preview.py
+++ backend/migrations/versions/20250213_add_metadata_fields_to_preview.py
@@ -1,7 +1,7 @@
 """add metadata fields to preview
 
 Revision ID: 20250213_add_metadata_fields_to_preview
-Revises: 20250201_add_description_to_preview
+Revises: 20250201_add_desc
 Create Date: 2025-02-13 00:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '20250213_add_metadata_fields_to_preview'
-down_revision: Union[str, None] = '20250201_add_description_to_preview'
+down_revision: Union[str, None] = '20250201_add_desc'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
